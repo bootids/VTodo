9208ff37e130ba0e34fdec8429acf8e6efde3607
Bootis <yxh1316185617@gmail.com>
2019-08-09 10:02:45 +0800

    add cloud config

--- config-server/src/main/java/indi/bootis/config/ConfigServerApplication.java
+++ config-server/src/main/java/indi/bootis/config/ConfigServerApplication.java
@@ -2,8 +2,17 @@ package indi.bootis.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author Bootis
+ */
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+@Configuration
 public class ConfigServerApplication {
 
   public static void main(String[] args) {
